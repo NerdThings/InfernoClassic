10c84eb5f9d2692d896164fd2b2fedab3163040b
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2018-08-27 11:42:10 +0100

    Fix multiplication logic and try division.

--- Inferno.Runtime/Matrix.cs
+++ Inferno.Runtime/Matrix.cs
@@ -286,48 +286,31 @@ namespace Inferno.Runtime
 
         public static Matrix operator /(Matrix a, Matrix b)
         {
-            return new Matrix()
-            {
-                M11 = a.M11 / b.M11,
-                M12 = a.M12 / b.M12,
-                M13 = a.M13 / b.M13,
-                M14 = a.M14 / b.M14,
-                M21 = a.M21 / b.M21,
-                M22 = a.M22 / b.M22,
-                M23 = a.M23 / b.M23,
-                M24 = a.M24 / b.M24,
-                M31 = a.M31 / b.M31,
-                M32 = a.M32 / b.M32,
-                M33 = a.M33 / b.M33,
-                M34 = a.M34 / b.M34,
-                M41 = a.M41 / b.M41,
-                M42 = a.M42 / b.M42,
-                M43 = a.M43 / b.M43,
-                M44 = a.M44 / b.M44,
-            };
+            //Needs verification
+            return a * b.Invert();
         }
 
         public static Matrix operator *(Matrix a, Matrix b)
         {
             return new Matrix()
             {
-                M11 = a.M11 * b.M11,
-                M12 = a.M12 * b.M12,
-                M13 = a.M13 * b.M13,
-                M14 = a.M14 * b.M14,
-                M21 = a.M21 * b.M21,
-                M22 = a.M22 * b.M22,
-                M23 = a.M23 * b.M23,
-                M24 = a.M24 * b.M24,
-                M31 = a.M31 * b.M31,
-                M32 = a.M32 * b.M32,
-                M33 = a.M33 * b.M33,
-                M34 = a.M34 * b.M34,
-                M41 = a.M41 * b.M41,
-                M42 = a.M42 * b.M42,
-                M43 = a.M43 * b.M43,
-                M44 = a.M44 * b.M44,
-            };
+                M11 = (((a.M11 * b.M11) + (a.M12 * b.M21)) + (a.M13 * b.M31)) + (a.M14 * b.M41),
+                M12 = (((a.M11 * b.M12) + (a.M12 * b.M22)) + (a.M13 * b.M32)) + (a.M14 * b.M42),
+                M13 = (((a.M11 * b.M13) + (a.M12 * b.M23)) + (a.M13 * b.M33)) + (a.M14 * b.M43),
+                M14 = (((a.M11 * b.M14) + (a.M12 * b.M24)) + (a.M13 * b.M34)) + (a.M14 * b.M44),
+                M21 = (((a.M21 * b.M11) + (a.M22 * b.M21)) + (a.M23 * b.M31)) + (a.M24 * b.M41),
+                M22 = (((a.M21 * b.M12) + (a.M22 * b.M22)) + (a.M23 * b.M32)) + (a.M24 * b.M42),
+                M23 = (((a.M21 * b.M13) + (a.M22 * b.M23)) + (a.M23 * b.M33)) + (a.M24 * b.M43),
+                M24 = (((a.M21 * b.M14) + (a.M22 * b.M24)) + (a.M23 * b.M34)) + (a.M24 * b.M44),
+                M31 = (((a.M31 * b.M11) + (a.M32 * b.M21)) + (a.M33 * b.M31)) + (a.M34 * b.M41),
+                M32 = (((a.M31 * b.M12) + (a.M32 * b.M22)) + (a.M33 * b.M32)) + (a.M34 * b.M42),
+                M33 = (((a.M31 * b.M13) + (a.M32 * b.M23)) + (a.M33 * b.M33)) + (a.M34 * b.M43),
+                M34 = (((a.M31 * b.M14) + (a.M32 * b.M24)) + (a.M33 * b.M34)) + (a.M34 * b.M44),
+                M41 = (((a.M41 * b.M11) + (a.M42 * b.M21)) + (a.M43 * b.M31)) + (a.M44 * b.M41),
+                M42 = (((a.M41 * b.M12) + (a.M42 * b.M22)) + (a.M43 * b.M32)) + (a.M44 * b.M42),
+                M43 = (((a.M41 * b.M13) + (a.M42 * b.M23)) + (a.M43 * b.M33)) + (a.M44 * b.M43),
+                M44 = (((a.M41 * b.M14) + (a.M42 * b.M24)) + (a.M43 * b.M34)) + (a.M44 * b.M44)
+            }
         }
 
         [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
