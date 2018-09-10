159f6562abe7a3b8b6fbb8d05021f8d84f8430ce
Rover656 <Rover656@users.noreply.github.com>
2018-09-10 20:59:44 +0100

    Add some basic stuff

--- Inferno.Runtime/Graphics/BaseTexture2D.cs
+++ Inferno.Runtime/Graphics/BaseTexture2D.cs
@@ -6,5 +6,17 @@ namespace Inferno.Runtime.Graphics
 {
     public abstract class BaseTexture2D
     {
+        // ReSharper disable once InconsistentNaming
+        internal int _width;
+        // ReSharper disable once InconsistentNaming
+        internal int _height;
+        internal int ArraySize;
+
+        public Rectangle Bounds => new Rectangle(0, 0, _width, _height);
+
+        public int Width => _width;
+        public int Height => _height;
+
+        //TODO: Proper Texture2D stuff (And native stuffs)
     }
 }
