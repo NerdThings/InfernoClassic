9f7167b198668e8d1cf9d72b2da7efbaf219b923
Rover656 <Rover656@users.noreply.github.com>
2018-09-22 22:00:31 +0100

    Commit some removals

--- Inferno.Runtime.Tests.Windows/Program.cs
+++ Inferno.Runtime.Tests.Windows/Program.cs
@@ -2,7 +2,6 @@
 
 namespace Inferno.Runtime.Tests.Windows
 {
-#if WINDOWS || LINUX
     /// <summary>
     /// The main class.
     /// </summary>
@@ -18,5 +17,4 @@ namespace Inferno.Runtime.Tests.Windows
                 game.Run();
         }
     }
-#endif
 }
